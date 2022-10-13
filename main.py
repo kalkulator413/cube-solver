7c4b959cfcc60c63a35fc1e9104e37bcea623801
Kalkin <50224596+kalkulator413@users.noreply.github.com>
2022-10-13 14:01:10 -0700

    clean up some code

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import cv2
 # define a video capture object
 vid = cv2.VideoCapture(0)
 
-# white = (255, 255, 255)
-# white = cv2.COLOR_RGB2HSV((255, 255, 255))
-black = (0, 0, 0)
-
 colors = {
     "blue":{
         "low":(84, 50, 0),
@@ -28,31 +24,22 @@ while(True):
 
     # invert frame
     frame = cv2.flip(frame, 1)
-
-    # print(frame.shape[0])
-    # print(frame.shape[1])
     
-    # for color in colors:
-    #     masked = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), colors[color]['low'] , colors[color]['high'])
-    #     contours, h = cv2.findContours(masked, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #     if len(contours) > 0:
-    #         # for x in contours:
-    #         #     print(x.shape[0]*x.shape[1])
-    #         contours = [x for x in contours if (x.shape[0]*x.shape[1] > 150)]
-    #         cv2.drawContours(frame, contours, -1, colors[color]['color'], 6)
-        
-    #     cv2.rectangle
+    # contour each color
+    for color in colors:
+        masked = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), colors[color]['low'] , colors[color]['high'])
+        contours, h = cv2.findContours(masked, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contours = [x for x in contours if (x.shape[0]*x.shape[1] > 150)]
+            cv2.drawContours(frame, contours, -1, colors[color]['color'], 6)
 
-
-    # for x in range(3):
-    #     for y in range(3):
-    #         cv2.rectangle(frame, (185+x*90, 105 + y*90), (185 + (x+1)*90, 105 + (y+1)*90), (50, 50, 50), 5)
+    # box for placing cube into
+    for x in range(3):
+        for y in range(3):
+            cv2.rectangle(frame, (185+x*90, 105 + y*90), (185 + (x+1)*90, 105 + (y+1)*90), (50, 50, 50), 5)
 
     cv2.imshow('frame', frame)
       
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
   
